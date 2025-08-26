bf7eddcf5adb4e91f5321c8c762f4bb81229dc07
Christopher S. Vesterby <vesterby@hotmail.dk>
2025-08-26 13:47:29 +0200

    Opgave 1 løst

---
+++ main.js
@@ -0,0 +1,15 @@
+// Opgave 1
+let person = {
+    firstname: "Christopher",
+    lastname: "Vesterby",
+    age: 29,
+    hometown: "Løvel",
+};
+
+// Udskrivning med dot notation
+console.log(person.firstname);
+console.log(person.lastname);
+
+// Udskrivning med bracket notation
+console.log(person["age"]);
+console.log(person["hometown"]);
